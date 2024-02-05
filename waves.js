e34de073bef841d6e778dec53862034184832356
manlabmanlab <74274515+manlabmanlab@users.noreply.github.com>
2024-02-05 15:46:11 -0500

    tree_cambios

--- waves.js
+++ waves.js
@@ -1,2 +1,5 @@
 cuberneters.com
 jerarquia de paises que sobresalen a las dificultades presentes
+documentando cambios en esta pagina
+al incorporar nuevos elementos en el document
+ingresamos extenso e n lineas EOF a fin de determinar mas elemntos.
